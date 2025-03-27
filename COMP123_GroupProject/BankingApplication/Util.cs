61bbe6837fde73419001ad5fef535124dcefd9e8
Mushfiqa1 <mbhuiy70@my.centennialcollege.ca>
2025-03-27 12:13:19 -0400

    Util class done

---
+++ COMP123_GroupProject/BankingApplication/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplication
+{
+    static class Util
+    {
+        private static Random random = new Random();
+
+        private static DateTime currentTime = new DateTime();
+    }
+}
