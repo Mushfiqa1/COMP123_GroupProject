c404005954dd29e1f91c79df4c45158b13d8c821
Mushfiqa1 <mbhuiy70@my.centennialcollege.ca>
2025-03-27 18:52:31 -0400

    Update on Util.cs

--- COMP123_GroupProject/BankingApplication/Util.cs
+++ COMP123_GroupProject/BankingApplication/Util.cs
@@ -10,6 +10,15 @@ namespace BankingApplication
     {
         private static Random random = new Random();
 
-        private static DateTime currentTime = new DateTime();
+        private static DayTime currentTime = new DayTime(1_154_005); // represents 2025-03-22 10:15
+
+        public static DayTime Now
+        {
+            get
+            {
+                currentTime += random.Next(100); // increment by 0 to 99 minutes
+                return currentTime;
+            }
+        }
     }
 }
